014d55839806e3aa62452520e05d33c1b8f6b47e
gevalinho <gevalinho1@gmail.com>
2025-04-22 16:42:02 +0100

    Implemting themes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold font-space-grotesk text-red-500">
+      <h1 className="text-3xl font-bold font-space-grotesk">
         what do we have here?
       </h1>
     </>
